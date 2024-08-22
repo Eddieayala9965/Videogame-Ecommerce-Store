4b1e4fa0b15a9f105e7eee6417dab0d986ef001b
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-08-22 15:45:47 -0400

    made changes to schemas.py, i made a mistake by not creating schemas for updating, so redid the all my schemas to make sure i reflect that when im creating my crud functions

--- Backend/app/crud.py
+++ Backend/app/crud.py
@@ -1,14 +1,27 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 from .models import User, Product, Order, Review
+from .schemas import UserCreate, UserOut, ProductBase, ProductOut, OrderBase, OrderOut, ReviewBase, ReviewOut
 from uuid import UUID
 
-async def get_user(db: AsyncSession, user_id: UUID):
-    return await db.get(User, user_id)
+async def get_user(db: AsyncSession, user_id: UUID) -> UserOut:
+    user = await db.get(User, user_id)
+    if not user:
+        return None
+    return UserOut.from_attributes(user)
 
-async def create_user(db: AsyncSession, user: User):
+async def get_user_by_email(db: AsyncSession, email: str) -> UserOut:
+    stmt = select(User).where(User.email == email)
+    result = await db.execute(stmt)
+    user = result.scalar().first()
+    return UserOut.from_attributes(user)
+
+async def create_user(db: AsyncSession, user_data: UserCreate) -> UserOut:
+    user = User(**user_data.model_dump())
     db.add(user)
     await db.commit()
     await db.refresh(user)
-    return user
+    return UserOut.from_attributes(user)
 
+async def update_user(db: AsyncSession, user_id: UUID, updated_user_data: UserCreate) -> UserOut:
 
--- Backend/app/schemas.py
+++ Backend/app/schemas.py
@@ -9,6 +9,13 @@ class UserBase(BaseModel):
 class UserCreate(UserBase):
     password: str
 
+class UserUpdate(BaseModel):
+    username: Optional[str]
+    email: Optional[EmailStr]
+    password: Optional[str]
+    is_active: Optional[bool] = True
+    profile_picture: Optional[str]
+
 class UserOut(UserBase):
     id: UUID
     is_active: bool
@@ -18,10 +25,20 @@ class UserOut(UserBase):
         from_attributes = True
 
 
+
 class ProductBase(BaseModel):
     name: str
     description: str
     price: float
+    image_url: Optional[str]
+
+class ProductCreate(ProductBase):
+    pass
+
+class ProductUpdate(BaseModel):
+    name: Optional[str]
+    description: Optional[str]
+    price: Optional[float]
     image_url: Optional[str]
 
 class ProductOut(ProductBase):
@@ -37,12 +54,17 @@ class OrderBase(BaseModel):
     quantity: int
     status: Optional[str] = "Pending"
 
-class OrderOut(BaseModel):
+class OrderCreate(OrderBase):
+    pass
+
+class OrderUpdate(BaseModel):
+    product_id: Optional[UUID]
+    quantity: Optional[int]
+    status: Optional[str]
+
+class OrderOut(OrderBase):
     id: UUID
     user_id: UUID
-    product_id: UUID
-    quantity: int
-    status: str
 
     class Config:
         from_attributes = True
@@ -52,6 +74,14 @@ class ReviewBase(BaseModel):
     product_id: UUID
     rating: int
     comment: str
+
+class ReviewCreate(ReviewBase):
+    pass
+
+class ReviewUpdate(BaseModel):
+    product_id: Optional[UUID]
+    rating: Optional[int]
+    comment: Optional[str]
 
 class ReviewOut(ReviewBase):
     id: UUID
