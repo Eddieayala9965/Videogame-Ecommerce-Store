caf51a7bf2f77c6b8ec93b033b27ca2b4271d022
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-08-22 16:18:07 -0400

    finished the user crud operations, just need to test them when im done with all the crud operations.

--- Backend/app/crud.py
+++ Backend/app/crud.py
@@ -1,8 +1,10 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from .models import User, Product, Order, Review
-from .schemas import UserCreate, UserOut, ProductBase, ProductOut, OrderBase, OrderOut, ReviewBase, ReviewOut
+from .schemas import UserCreate, UserUpdate, UserOut, ProductBase, ProductCreate, ProductUpdate, ProductOut, OrderBase, OrderCreate, OrderUpdate, OrderOut, ReviewBase, ReviewCreate, ReviewUpdate, ReviewOut
 from uuid import UUID
+
+# user crud operations
 
 async def get_user(db: AsyncSession, user_id: UUID) -> UserOut:
     user = await db.get(User, user_id)
@@ -13,7 +15,7 @@ async def get_user(db: AsyncSession, user_id: UUID) -> UserOut:
 async def get_user_by_email(db: AsyncSession, email: str) -> UserOut:
     stmt = select(User).where(User.email == email)
     result = await db.execute(stmt)
-    user = result.scalar().first()
+    user = result.scalars().first()
     return UserOut.from_attributes(user)
 
 async def create_user(db: AsyncSession, user_data: UserCreate) -> UserOut:
@@ -23,5 +25,21 @@ async def create_user(db: AsyncSession, user_data: UserCreate) -> UserOut:
     await db.refresh(user)
     return UserOut.from_attributes(user)
 
-async def update_user(db: AsyncSession, user_id: UUID, updated_user_data: UserCreate) -> UserOut:
+async def update_user(db: AsyncSession, user_id: UUID, updated_user_data: UserUpdate) -> UserOut:
+    user = await get_user(db, user_id)
+    if user:
+        for key, value in updated_user_data.model_dump(exclude_unset=True).items():
+            setattr(user, key, value)
+    await db.commit()
+    await db.refresh(user)
+    return UserOut.from_attributes(user)
 
+async def delete_user(db: AsyncSession, user_id: UUID) -> UserOut:
+    user = await get_user(db, user_id)
+    if user:
+        await db.delete(user)
+        await db.commit()
+    return UserOut.from_attributes(user)
+
+
+# product crud operations
