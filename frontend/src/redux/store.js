b7c9ace3efb99b85b8935c7a63775ed3e15a86b5
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-08-29 14:35:26 -0400

    finished coding store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,2 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./slices/";
+import authReducer from "./slices/authSlice";
+import cartReducer from "./slices/cartSlice";
+import orderReducer from "./slices/orderSlice";
+import reviewReducer from "./slices/reviewSlice";
+import userReducer from "./slices/userSlice";
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    cart: cartReducer,
+    order: orderReducer,
+    review: reviewReducer,
+    user: userReducer,
+  },
+});
+
+export default store;
