c986e47bc102e4ed2d16ba44a983790d8994ecb3
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-08-31 17:34:41 -0400

    needed to adjust mu auth.py's login route

--- Backend/app/api/endpoints/auth.py
+++ Backend/app/api/endpoints/auth.py
@@ -97,6 +97,21 @@ async def register(user: schemas.UserCreate, db: AsyncSession = Depends(get_db))
         raise HTTPException(status_code=400, detail="Username already registered")
     return await create_user(db=db, user=user)
 
+# @router.post("/login", response_model=dict)
+# async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)):
+#     user = await get_user_by_username(db, username=form_data.username)
+#     if not user or not verify_password(form_data.password, user.hashed_password):
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail="Incorrect username or password",
+#             headers={"WWW-Authenticate": "Bearer"},
+#         )
+#     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+#     access_token = create_access_token(
+#         data={"sub": user.username}, expires_delta=access_token_expires
+#     )
+#     return {"access_token": access_token, "user_id": str(user.id)}
+
 @router.post("/login", response_model=dict)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)):
     user = await get_user_by_username(db, username=form_data.username)
@@ -110,8 +125,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
-    return {"access_token": access_token, "token_type": "bearer"}
-
+    return {"access_token": access_token, "user_id": str(user.id)}
 
 @router.put("/update_user/{user_id}", response_model=schemas.UserOut)
 async def update_user(user_id: uuid.UUID, user: schemas.UserUpdate, db: AsyncSession = Depends(get_db)):
