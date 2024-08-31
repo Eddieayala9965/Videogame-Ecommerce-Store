ab5a93d587f3f29d19c8bbcc463bc099b4b57122
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-08-30 22:12:03 -0400

    finished coding all esential endpoints in my api.js file

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -27,4 +27,74 @@ api.interceptors.response.use(
   }
 );
 
+// Auth endpoints
+
+export const registerUser = async (userData) => {
+  return api.post("/auth/register", userData);
+};
+
+export const loginUser = async (loginData) => {
+  return api.post("/auth/login", loginData);
+};
+
+// Cart endpoints
+
+export const getCartItems = async (userId) => {
+  return api.get(`/cart?user_id=${userId}`);
+};
+
+export const addToCart = async (cartItem, userId) => {
+  return api.post(`/cart/${userId}`, cartItem);
+};
+
+export const updateCartItem = async (cartId, cartItem) => {
+  return api.put(`/cart/${cartId}`, cartItem);
+};
+
+export const deleteCartItem = async (cartId) => {
+  return api.delete(`/cart/${cartId}`);
+};
+
+// order endpoints
+
+export const createOrder = async (orderData, userId) => {
+  return api.post(`/orders`, { user_id: userId, ...orderData });
+};
+
+export const getUserOrders = async (userId) => {
+  return api.get(`/orders?user_id=${userId}`);
+};
+
+export const updateOrderStatus = async (orderId, orderData) => {
+  return api.put(`/orders/${orderId}`, orderData);
+};
+
+export const deleteOrder = async (orderId) => {
+  return api.delete(`/orders/${orderId}`);
+};
+
+// review endpoints
+
+export const createReview = async (reviewData, userId) => {
+  return api.post(`/reviews`, { user_id: userId, ...reviewData });
+};
+
+export const getReviewsForGame = async (gameID) => {
+  return api.get(`/reviews/game/${gameID}`);
+};
+
+export const updateReview = async (reviewId, reviewData) => {
+  return api.put(`/reviews/${reviewId}`, reviewData);
+};
+
+export const deleteReview = async (reviewId) => {
+  return api.delete(`/reviews/${reviewId}`);
+};
+
+// game search endpoint
+
+export const searchGames = async (query) => {
+  return api.get(`/games/search`, { params: { query } });
+};
+
 export default api;
