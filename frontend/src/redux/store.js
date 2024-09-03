20d985e1b591fc52700d654b632db1e2d58e86d9
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-09-02 23:37:21 -0400

    nvrm i fixed the issue, just forgot to delete in my store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,7 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import cartReducer from "./slices/cartSlice";
 import orderReducer from "./slices/orderSlice";
-import reviewReducer from "./slices/reviewSlice";
 import userReducer from "./slices/userSlice";
 
 const store = configureStore({
@@ -10,7 +9,6 @@ const store = configureStore({
     auth: authReducer,
     cart: cartReducer,
     order: orderReducer,
-    review: reviewReducer,
     user: userReducer,
   },
 });
