cf0558b3d10d8de6334e1f63c41e7c34d13e53fe
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-09-03 23:04:23 -0400

    another attempt

--- Backend/app/db.py
+++ Backend/app/db.py
@@ -1,15 +1,31 @@
-import os
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+# import os
+# from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+# from sqlalchemy.orm import sessionmaker
+# from dotenv import load_dotenv
+
+
+
+# load_dotenv()
+
+# DATABASE_URL = os.getenv("DATABASE_URL")
+
+# engine = create_async_engine(DATABASE_URL, echo=True)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+
+# async def get_db():
+#     async with SessionLocal() as session:
+#         yield session
+
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-
-
-
-load_dotenv()
+import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# Async engine
 engine = create_async_engine(DATABASE_URL, echo=True)
+
+# Async session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
 
 async def get_db():
